9075970c374319a215e1d767a0355ba2b3a83c6d
nutchanonproject <nutzbyte@gmail.com>
2016-12-02 10:31:53 +0700

    heading
    heading with dashed line.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Heading Test</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="container">
+		<div class="feature-title">
+			<div class="fancy-title">
+				<span class="line-dashed line-left"></span>
+					<h3>Heading and Line Testing</h3>
+				<span class="line-dashed line-right"></span>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
